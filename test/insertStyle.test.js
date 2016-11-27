2bc37187d0bd062157808a8f7e90d65695925920
Justin Hall <justin.r.hall@gmail.com>
2016-11-27 15:13:51 -0700

    Fix and reorganize certain tests (#41)
    * Prevent insertStyle tests from trampling global.document
    
    We were just lucky that these happened to occur after our other tests which
    depend on global.document (initialized in test/.setup.js). Enzyme tests will
    blow up if the global document object is deleted. This change preserves the
    value up front and restores it when leaving the test group.
    
    * Rename and reorganize certain tests
    
    * Rename test files to match functions tested rather than filenames
    * Reorganize withStyles tests into clearer groupings

--- test/insertStyle.test.js
+++ test/insertStyle.test.js
@@ -3,7 +3,13 @@ import { expect } from 'chai';
 
 const cacheKey = require.resolve('../src/insert-style');
 
-describe('insert-style', () => {
+describe('insertStyle', () => {
+  const document = global.document;
+
+  after(() => {
+    global.document = document;
+  });
+
   beforeEach(() => {
     if (require.cache[cacheKey]) {
       delete require.cache[cacheKey];
@@ -16,12 +22,12 @@ describe('insert-style', () => {
       const insertStyle = require('../src/insert-style');
 
       expect(insertStyle.__get__('isServer')).to.equal(false);
-      delete global.document;
     });
   });
 
   describe('when on the server', () => {
     it('should return Server variant', () => {
+      delete global.document;
       const insertStyle = require('../src/insert-style');
 
       expect(insertStyle.__get__('isServer')).to.equal(true);
